6f1404636ffabd7f143ef3d4a63d185ecd2f404f
shalomtash <shalomtash93@gmail.com>
2022-06-24 08:19:54 +0000

    counter challenge task

---
+++ assets/main.js
@@ -0,0 +1,26 @@
+const button5 = document.querySelector("#plusFive")
+const indicator = document.querySelector("#number-indicator")
+const disp = document.querySelector("#display-heading")
+const button10 = document.getElementById("plusTen")
+const button15 = document.getElementById("plusFifteen")
+const button20 = document.getElementById("plusTwenty")
+
+button5.addEventListener('click', () => {
+  disp.textContent = "added 5"
+  indicator.textContent = (parseInt(indicator.textContent) + 5).toString()
+})
+
+button10.addEventListener('click', ()=>{
+  disp.textContent = "added 10"
+  indicator.textContent = (parseInt(indicator.textContent) + 10).toString() 
+})
+
+button15.addEventListener('click', ()=>{
+  disp.textContent = "added 15"
+  indicator.textContent = (parseInt(indicator.textContent) + 15).toString() 
+})
+
+button20.addEventListener('click', ()=>{
+  disp.textContent = "added 20"
+  indicator.textContent = (parseInt(indicator.textContent) + 20).toString() 
+})
